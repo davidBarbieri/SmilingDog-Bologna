c4e029969bd2f20b9a4d535b29d1fea99deb1dd1
Lucas Ramos <ciao@lucasramos.me>
2017-06-17 23:40:15 +0200

    website tested

--- releases/SmilingDog-Bologna-0.0.1/php/templates/intros/home.php
+++
@@ -1,8 +0,0 @@
-<div class="intro carousel">
-    <div class="slider">
-        <img src="/img/home/agility-1.jpg" alt="Agility Bologna, Emilia Romagna" height="683" width="1024">
-        <img src="/img/home/obedience-2.jpg" alt="Obedience Bologna, Emilia Romagna" height="683" width="1024">
-        <img src="/img/home/agility-3.jpg" alt="Agility a Bologna, Emilia Romagna" height="683" width="1024">
-        <img src="/img/home/puppy_class-4.jpg" alt="Educazione di base cani e puppy class a Bologna, Emilia Romagna" height="683" width="1024">
-    </div>
-</div>
--- releases/latest/php/templates/head.php
+++ releases/latest/php/templates/head.php
@@ -1,10 +1,30 @@
+<?php
+    $sectioArr = array(
+        "home" => "",
+        "chi-siamo" => "Chi siamo |",
+        "corsi" => "Corsi |",
+        "contatti" => "Dove siamo - Contatti |"
+    );
+?>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>Smiling Dob - Bologna</title>
+<title><?php echo $sectioArr[$_GET['page']]; ?> Smiling Dog - Bologna</title>
+
+<!-- meta og -->
 <meta name="keyowords" content="educazione cinofila bologna, educatore cinofilo bologna, educazione cani bologna, educatrice cinofila, cristina collina, marina flammini, obedience bologna, agility bologna, rally obedience bologna, puppy class bologna, cae-1 enci bologna, enci bologna, problemi comportamentali bologna, preparazione cani disabili bologna, centro cinofilo bologna, cani bologna, educazione cinofila, educatore cinofilo, educazione cani, educatrice cinofila, obedience, agility, rally obedience, puppy class, cae-1 enci bologna, enci, problemi comportamentali, preparazione cani disabili, centro cinofilo, cane, cani">
 <meta name="description" content="Il Centro Cinofilo Smiling Dog Bologna nasce dalla passione per la cinofilia sviluppata in anni di lavoro con i nostri cani sotto la guida di Daniela Scanelli, responsabile del Centro Cinofilo Smiling Dog.
     La nostra associazione promuove lo sviluppo di una corretta relazione tra uomo e cane basata sul rispetto e sulla collaborazione.">
 <meta name="viewport" content="width=device-width, initial-scale=1, initial-scale=1, user-scalable=no">
+<link rel="canonical" href="http://www.smilingdogbologna.it/">
+<link rel="image_src" type="image/jpeg" href="http://www.smilingdogbologna.it/img/logo_smilingdog-bologna.jpg">
+<!-- end meta -->
+
+<!-- meta og -->
+<meta property="og:image" content="http://www.smilingdogbologna.it/img/logo_smilingdog-bologna.jpg">
+<meta property="og:type" content="article">
+<meta property="og:title" content="Smiling Dog Bologna">
+<meta property="og:url" content="http://www.smilingdogbologna.it/">
+<!-- end meta og -->
 
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400|Quicksand:400,700">
