83853ad9086724075c7842e03ebf0fd1f5a71964
Lucas Ramos <lucas_r@me.com>
2018-11-13 18:11:02 +0100

    db

--- dev/php/dal/db.php
+++ dev/php/dal/db.php
@@ -1,9 +1,9 @@
 <?php
     /* DB */
-    define("DB_HOST", "localhost");
-    define("DB_NAME", "smilingdog_bologna");
-    define("DB_USER", "smilingdog_user");
-    define("DB_PASS", "mojitoedora");
+    define("DB_HOST", "89.46.111.75");
+    define("DB_USER", "Sql1267673");
+    define("DB_PASS", "21z31154nm");
+    define("DB_NAME", "Sql1267673_1");
 
     /* RETRIEVING DATA */
     $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
