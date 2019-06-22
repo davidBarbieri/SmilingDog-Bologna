86b487789c5dce02f156fc37dd6d4339d6a222d2
David Barbieri <david.barbieri@ynap.com>
2019-06-22 14:54:42 +0200

    fix 404 keywords and sitemap

--- dev/php/templates/head.php
+++ dev/php/templates/head.php
@@ -13,7 +13,7 @@
 <title><?php echo $sectioArr[$_GET['page']]; ?>Centro Cinofilo Smiling Dog - Bologna</title>
 
 <!-- meta og -->
-<meta name="keyowords" content="educazione cinofila bologna, educatore cinofilo bologna, educazione cani bologna, educatrice cinofila, cristina collina, marina flammini, obedience bologna, agility bologna, rally obedience bologna, puppy class bologna, cae-1 enci bologna, enci bologna, problemi comportamentali bologna, preparazione cani disabili bologna, centro cinofilo bologna, cani bologna, educazione cinofila, educatore cinofilo, educazione cani, educatrice cinofila, obedience, agility, rally obedience, puppy class, cae-1 enci bologna, enci, problemi comportamentali, preparazione cani disabili, centro cinofilo, cane, cani">
+<meta name="keywords" content="educazione cinofila bologna, educatore cinofilo bologna, educazione cani bologna, educatrice cinofila, cristina collina, marina flammini, obedience bologna, agility bologna, rally obedience bologna, puppy class bologna, cae-1 enci bologna, enci bologna, problemi comportamentali bologna, preparazione cani disabili bologna, centro cinofilo bologna, cani bologna, educazione cinofila, educatore cinofilo, educazione cani, educatrice cinofila, obedience, agility, rally obedience, puppy class, cae-1 enci bologna, enci, problemi comportamentali, preparazione cani disabili, centro cinofilo, cane, cani">
 <meta name="description" content="Il Centro Cinofilo Smiling Dog Bologna promuove lo sviluppo di una corretta relazione tra uomo e cane basata sul rispetto e sulla collaborazione.">
 <meta name="viewport" content="width=device-width, initial-scale=1, initial-scale=1, user-scalable=no">
 <link rel="canonical" href="http://www.smilingdogbologna.it/">
